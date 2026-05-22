8012d91c54558b74352d75faa838c944a6579f63
Maysa853 <galvaomaysa6@gmail.com>
2026-05-22 17:48:46 -0300

    Aula2205- Server index

--- main.py
+++ main.py
@@ -6,7 +6,12 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template('index.html')
+    nome = 'Feirascore'
+    return render_template('index.html', site = nome)
+    
+@app.route("/login")
+def login():
+    return render_template('login/login.html')
 
 def main():
     app.run(port=int(os.environ.get('PORT', 80)))
