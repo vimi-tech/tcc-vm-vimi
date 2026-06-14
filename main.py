5ebcb193f0e91cbe4d3aa8a255bc4c13d47e2940
Maysa853 <galvaomaysa6@gmail.com>
2026-06-14 14:11:54 -0300

    Atualização Login

--- main.py
+++ main.py
@@ -1,29 +1,21 @@
 import os
-
 from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
-
 @app.route("/")
 def index():
     nome = 'Feirascore'
-    return render_template('index.html', site = nome)
-   
-# Certifique-se de que não há espaços antes do '@' e do 'def'
-@app.route('/loginestudante')
-def loginestudante():
-    # Esse nome tem que ser idêntico ao do arquivo salvo!
-    return render_template('loginestudante.html')
+    return render_template('index.html', site=nome)
+
+@app.route('/estudantelogin')
+def estudantelogin():
+    # Certifique-se de que o arquivo loginestudante.html está na pasta correta
+    return render_template('login/estudantelogin.html')
 
 @app.route("/login")
 def login():
     return render_template('login/login.html')
 
-def main():
-    app.run(port=int(os.environ.get('PORT', 80)))
-
 if __name__ == "__main__":
-    main()
-
-
+    app.run(debug=True)
