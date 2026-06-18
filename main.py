e9856b331707cc9c72057d91ec0941b521bc232b
isamatosnascimento16-lgtm <isamatosnascimento16@gmail.com>
2026-06-17 23:10:22 -0300

    17/06

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ def index():
 
 @app.route('/estudantelogin')
 def estudantelogin():
-    # Certifique-se de que o arquivo loginestudante.html está na pasta correta
+
     return render_template('login/estudantelogin.html')
 
 @app.route("/login")
@@ -18,4 +18,45 @@ def login():
     return render_template('login/login.html')
 
 if __name__ == "__main__":
+    app.run(debug=True)
+
+    @app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/estandes')
+def estandes():
+    return render_template('estandes.html')
+
+
+@app.route('/noticias')
+def noticias():
+    return render_template('noticias.html')
+
+
+@app.route('/localizacao')
+def localizacao():
+    return render_template('localizacao.html')
+
+
+@app.route('/faq')
+def faq():
+    return render_template('faq.html')
+
+
+
+@app.route('/login')
+def login():
+  
+    return render_template('login.html')
+
+@app.route('/estudantelogin')
+def estudantelogin():
+   
+    return render_template('estudantelogin.html')
+
+
+if __name__ == '__main__':
+  
     app.run(debug=True)
